4ba8b7e25877b988c53180fdcf19e8def4ee2180
Julian George <jules104@gmail.com>
2019-12-27 16:57:14 +0000

    Add initialize method to store bikes

--- spec/docuking_station_spec.rb
+++ spec/docuking_station_spec.rb
@@ -2,17 +2,6 @@ require 'docking_station'
 
 describe DockingStation do  
     
-    it 'docks something' do
-        bike = Bike.new
-        expect(subject.dock(bike)).to eq bike
-    end
-
-    it 'returs docked bikes' do
-        bike = Bike.new
-        subject.dock(bike)
-        expect(subject.bike).to eq bike
-    end
-
     describe '#release_bike' do
 
     it { is_expected.to respond_to :release_bike }
@@ -25,7 +14,7 @@ describe DockingStation do
 
     describe '#dock' do
         it 'raises an error when full' do
-            subject.dock(Bike.new)
+            20.times { subject.dock Bike.new }
             expect { subject.dock Bike.new }.to raise_error 'Docking station full'
         end    
     end
